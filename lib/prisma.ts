d60bb1c0f1a3c5a79cdaec2e40bb290e75706ab6
geniuscode124 <abdurrahmanbabayemi@gmail.com>
2026-01-02 23:54:14 +0100

    refactor: Change the database to postgreSQL

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,16 @@
-import 'server-only';
-
 import { PrismaClient } from '../prisma/generated/prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const url = process.env.DATABASE_URL?.replace('file:', '') || './dev.db';
-const adapter = new PrismaBetterSqlite3({ url });
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
 
-const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma;
+export default prisma
